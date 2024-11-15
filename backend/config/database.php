40987a23b9c6fad4fd59325271432b8dfc3e5650
juanpalegre <juan.pablo.alegre@it4w.net>
2024-11-15 16:19:05 -0300

    database connect refactor

---
+++ backend/config/database.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->load();
+
+$servername = $_ENV['DB_HOST'];
+$port = $_ENV['DB_PORT'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
